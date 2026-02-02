1f06820805de42e197662d855962fee499842f6d
David Peter <davidnobody0108@gmail.com>
2026-02-02 19:49:36 +0100

    git commit -m "feat: add Navbar and Footer components, update layout and styles"

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+'use client'
+import Image from 'next/image'
+import React from 'react'
+import styles from './Navbar.module.css'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    // logo
+    <main className={styles.nav}>
+        <div className={styles.navImg}><Image src="/logo-dark.svg" alt="Logo" width={190} height={400} /></div>
+
+    {/* links */}
+
+        <div className={styles.navLinks}>
+            <Link className={styles.navLinks1} href="#" style={{marginRight: '20px', color: '#bcbcbc'}}>Home</Link>
+            <Link className={styles.navLinks2} href="#" style={{marginRight: '20px', color: '#bcbcbc'}}>About</Link>
+            <Link className={styles.navLinks3} href="#" style={{marginRight: '20px',  color: '#bcbcbc'}}>Contact</Link>
+            <Link className={styles.navLinks4} href="#" style={{marginRight: '20px', color: '#bcbcbc'}}>Blog</Link>
+            <Link className={styles.navLinks5} href="#" style={{color: '#bcbcbc'}}>Careers</Link>
+        </div>
+
+        {/* Request invite button */}
+        <div className={styles.requestInvite}>
+            <button className={styles.btn}>Request Invite</button>
+        </div>
+
+    </main>
+  )
+}
+
+export default Navbar
